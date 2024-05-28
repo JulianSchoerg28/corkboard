0c682c27dd73209e50c5e9c254de5d764aa8b37a
Julian <schoerg.julian@gmail.com>
2024-05-28 13:04:24 +0200

    basic server works

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log("Server läuft unter http://localhost:3000");
+});
